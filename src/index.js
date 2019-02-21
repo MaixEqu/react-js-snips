4f1cab84c1fb3add01c53a60a12df50224805d14
Debian live <eee1@i.ua>
2019-02-21 15:31:51 +0200

    React Ref - createRef (J2L, v.4+)

--- src/index.js
+++ src/index.js
@@ -3,27 +3,26 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.textInput1 = React.createRef();
-    this.textInput2 = React.createRef();
+    this.textInput_1 = React.createRef();
+    this.textInput_2 = React.createRef();
     this.state = {
-      value1: 'val 1',
-      value2: 'val 2',
+      value_1: 'val 1',
+      value_2: 'val 2',
       cols: 40,
       rows: 7,
     }
   }
 
-  handleSubmit1 = e => {
+  handleSubmit_1 = e => {
     e.preventDefault();
-    this.setState({ value1: this.textInput1.current.value})
+    this.setState({ value_1: this.textInput_1.current.value})
   };
-  handleSubmit2 = e => {
+  handleSubmit_2 = e => {
     e.preventDefault();
-    this.setState({ value1: this.textInput2.current.value})
+    this.setState({ value_2: this.textInput_2.current.value})
   };
   handleOnChange = e => {
     //e.preventDefault();
@@ -35,15 +34,20 @@ class App extends React.Component {
     const s = this.state;
     return (
       <div>
-        <h3>React Ref - createRef (J2L, v.4)</h3>
-        <textarea rows={s.rows} cols={s.cols} value={this.state.value1} onChange={this.handleOnChange} readOnly={true} />
-        <h3>Value2: {this.state.value2}</h3>
-        <form onSubmit={this.handleSubmit1}>
-          <input type="text" ref={this.textInput1} defaultValue="input 1" />
+        <h3>React Ref - createRef (J2L, v.4+)</h3>
+        <textarea rows={s.rows} cols={s.cols} 
+          value={this.state.value_1} 
+          onChange={this.handleOnChange} 
+          readOnly={true} 
+        />
+        <form onSubmit={this.handleSubmit_1}>
+          <textarea rows={s.rows} cols={s.cols} ref={this.textInput_1} defaultValue="input-1" />
+          <br />
           <button>Submit</button>
         </form>
-        <form onSubmit={this.handleSubmit2}>
-          <textarea rows={s.rows} cols={s.cols} ref={this.textInput2} defaultValue="input 2" />
+        <h3>Value 2: {this.state.value_2}</h3>
+        <form onSubmit={this.handleSubmit_2}>
+          <input type="text" ref={this.textInput_2} defaultValue="input-2" />
           <button>Submit</button>
         </form>
       </div>
