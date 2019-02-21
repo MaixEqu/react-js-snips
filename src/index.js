4d0b531cd900efdfae77a7724f845da2e2854bd2
Debian live <eee1@i.ua>
2019-02-21 13:54:44 +0200

    React Ref - createRef (J2L)

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
-// sources:
-// https://stackoverflow.com/questions/37949981/call-child-method-from-parent
-// https://codesandbox.io/s/8lxr0pq179
+// https://webformyself.com/rabota-s-ref-v-react/
 
-import React, { forwardRef, useRef, useImperativeHandle } from "react";
-import { render } from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-// We need to wrap component in `forwardRef` in order to gain
-// access to the ref object that is assigned using the `ref` prop.
-// This ref is passed as the second parameter to the function component.
-const Child = forwardRef((props, ref) => {
-  // The component instance will be extended
-  // with whatever you return from the callback passed
-  // as the second argument
-  useImperativeHandle(ref, () => ({
-    getAlert() {
-      alert("getAlert from Child");
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.textInput = React.createRef();
+    this.state = {
+      value: ''
     }
-  }));
+  }
 
-  return <h1>Hi Mx (J2L)</h1>;
-});
-
-const Parent = () => {
-  // In order to gain access to the child component instance,
-  // you need to assign it to a `ref`, so we call `useRef()` to get one
-  const childRef = useRef();
-
-  return (
-    <div>
-      <Child ref={childRef} />
-      <button onClick={() => childRef.current.getAlert()}>Click</button>
-    </div>
-  );
+handleSubmit = e => {
+  e.preventDefault();
+  this.setState({ value: this.textInput.current.value})
 };
 
-const rootElement = document.getElementById("root");
-render(<Parent />, rootElement);
+render() {
+  return (
+    <div>
+      <h1>React Ref - createRef (J2L)</h1>
+      <h3>Value: {this.state.value}</h3>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" ref={this.textInput} />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
