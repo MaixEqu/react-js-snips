2e2b7b584ff69b7efa6ff95eafb997505572bdc1
Debian live <eee1@i.ua>
2019-02-21 15:23:44 +0200

    React Ref - createRef (J2L, v.4)

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ class App extends React.Component {
     this.state = {
       value1: 'val 1',
       value2: 'val 2',
+      cols: 40,
+      rows: 7,
     }
   }
 
@@ -21,21 +23,27 @@ class App extends React.Component {
   };
   handleSubmit2 = e => {
     e.preventDefault();
-    this.setState({ value2: this.textInput2.current.value})
+    this.setState({ value1: this.textInput2.current.value})
   };
+  handleOnChange = e => {
+    //e.preventDefault();
+    //alert("onChange")
+    return true;
+  }
 
   render() {
+    const s = this.state;
     return (
       <div>
-        <h1>React Ref - createRef (J2L, v.3)</h1>
-        <h3>Value1: {this.state.value1}</h3>
+        <h3>React Ref - createRef (J2L, v.4)</h3>
+        <textarea rows={s.rows} cols={s.cols} value={this.state.value1} onChange={this.handleOnChange} readOnly={true} />
         <h3>Value2: {this.state.value2}</h3>
         <form onSubmit={this.handleSubmit1}>
           <input type="text" ref={this.textInput1} defaultValue="input 1" />
           <button>Submit</button>
         </form>
         <form onSubmit={this.handleSubmit2}>
-          <textarea ref={this.textInput2} defaultValue="input 2" />
+          <textarea rows={s.rows} cols={s.cols} ref={this.textInput2} defaultValue="input 2" />
           <button>Submit</button>
         </form>
       </div>
